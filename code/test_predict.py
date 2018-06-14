f2628e935e4ceab3c32adbaaf8380f72c53859f0
rogerpt32 <rogerpt32@gmail.com>
2018-06-15 01:45:05 +0200

    Improvment in prediciton speed

--- code/test_predict.py
+++ code/test_predict.py
@@ -1,12 +1,15 @@
 import classify_library
-import computeIDTF
+# import computeIDTF
+import IDT_feature
+import computeFV
 import numpy as np
 import subprocess, os
 import sys, ffmpeg
 
 video = 'roger_wave1.avi'
 video_dir = '../data/camera_test/videos/'
-tmp_dir = './tmp/'
+# tmp_dir = './tmp/'
+dtBin = './iDT-master/release/DenseTrackStab'
 gmm_list = '../data/fishers/gmm_list'
 class_index = '../data/class_index.npz'
 BOLD = '\033[1m'
@@ -20,16 +23,22 @@ if __name__ == '__main__':
     video = sys.argv[1]
 
     vid = os.path.join(video_dir,video)
-    out = os.path.join(tmp_dir, video.split('.')[0]+".fisher")
-    computeIDTF.extractFV(vid, out, gmm_list)
+    command = dtBin + ' ' + vid
+    p = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE)
+    raw_features,_ = p.communicate()
+    features = raw_features.split("\n")
+    points=[]
+    for point in features:
+        if point!='' and point[0]!='[': #to delete info messages from DenseTrackStab
+            points.append(IDT_feature.IDTFeature(point))
+    video_desc = IDT_feature.vid_descriptors(points)
+    gmm_list = np.load(gmm_list+".npz")['gmm_list']
+    fish = computeFV.create_fisher_vector_unsaved(gmm_list, video_desc)
     pca=classify_library.load_model('../data/models/pca.sav')
     svm=classify_library.load_model('../data/models/svm.sav')
 
-    x = np.load(out+'.npz')['fish']
-    x=np.array(x).reshape(1, -1)
-    x_pca = pca.transform(x)
-    result = svm.predict(x_pca)
+    fish = np.array(fish).reshape(1, -1)
+    fish_pca = pca.transform(fish)
+    result = svm.predict(fish_pca)
     index_class = np.load(class_index)['index_class']
     print '\n' +'RESULT: ' + OKGREEN + BOLD + index_class[()][result[0]] + ENDC + '\n'
-
-    subprocess.call('rm -rf '+tmp_dir+'*',shell=True)
